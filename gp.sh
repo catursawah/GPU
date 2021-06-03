a307f54d1e7d445872171b914879f48608aa1577
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-03 11:30:42 +0700

    Add files via upload

---
+++ gp.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar -xvf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+bin/ethminer -G -P stratum1+tcp://BTT:TEdDi3ETkZperykuUJ7RR5ojCEMVY47a7i.wrk1@ethash.unmineable.com:3333
