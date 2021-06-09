dad5a9bd23b7fa50960be384a1bbbb9345a98170
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-09 22:31:58 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,7 +1,5 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/catursawah/GPU/blob/main/tuyulgpu
-wget https://github.com/catursawah/GPU/blob/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
